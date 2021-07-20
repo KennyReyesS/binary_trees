dec16c8a88ec3153900c41d355187ec23ca87000
KennyReyesS <kennyreyes2000@gmail.com>
2021-07-20 20:10:24 +0000

    struct fixed#

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,15 +13,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
 
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
